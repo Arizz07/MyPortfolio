df015c53fd57a450115c5797a4afc9366720b763
Azz <arisachi7201@gmail.com>
2025-08-17 07:39:54 +0530

    added contact link

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
         <div className='flex gap-8 mr-4 '>
           <div className="flex gap-4 sections mt-1 mx-4 ">
             <span className='px-2 hover:font-semibold hover:border-b-4 hover:transition duration-200 hover:border-blue-300'><Link href={'#about'}>About</Link></span>
-            <span className='px-2 hover:font-semibold hover:border-b-4 hover:transition duration-200 hover:border-blue-300'><Link href={'/about'}>Contact</Link></span>
+            <span className='px-2 hover:font-semibold hover:border-b-4 hover:transition duration-200 hover:border-blue-300'><Link href={'#contact'}>Contact</Link></span>
             <span className='px-2 hover:font-semibold hover:border-b-4 hover:transition duration-200 hover:border-blue-300'><Link href={'/about'}>CV</Link></span>
           </div>
          <button>
